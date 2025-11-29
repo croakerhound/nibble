d7b7ae2c65a6b15bc3eeea83713e9e8184ce0a33
Reaper <reaperhound69004@gmail.com>
2025-11-29 23:02:50 +0530

    Added the connection to another func

--- main.go
+++ main.go
@@ -4,40 +4,15 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/charmbracelet/lipgloss"
-
 	"github.com/redis/go-redis/v9"
 )
-
-var style = lipgloss.NewStyle().
-	Bold(true).
-	Foreground(lipgloss.Color("#FAFAFA")).
-	Background(lipgloss.Color("#7D56F4")).
-	PaddingTop(2).
-	PaddingLeft(4).
-	Width(22)
 
 func main() {
 	// fmt.Println(style.Render("Redis TUI"))
 	name := scanText("Your name")
 	fmt.Println("Hello", name)
 
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password
-		DB:       0,  // use default DB
-		Protocol: 2,
-	})
-
-	ctx := context.Background()
-	pong, err := rdb.Ping(ctx).Result()
-
-	if err != nil {
-		fmt.Println("Failed to connect:", err)
-		return
-	}
-
-	fmt.Println("Connected to Redis:", pong)
+	_, _, _ = connectToRedis()
 
 }
 
@@ -48,4 +23,25 @@ func scanText(outputTxt string) string {
 	fmt.Scanln(&inputText)
 
 	return inputText
+}
+
+func connectToRedis() (*redis.Client, context.Context, error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password
+		DB:       0,  // use default DB
+		Protocol: 2,
+	})
+
+	ctx := context.Background()
+
+	pong, err := rdb.Ping(ctx).Result()
+
+	if err != nil {
+		return nil, nil, err
+	} else {
+		fmt.Println(pong)
+	}
+
+	return rdb, ctx, err
 }
